4ce678d307ac37f2e49d6bc40f466139746b5f5a
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-10-09 23:17:44 -0300

    add header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FiLogOut, FiUser } from "react-icons/fi";
 
 export default function Home() {
   return (
     <main>
-      <h1>TESTE</h1>
+      TESTE
     </main>
   );
 }
