d99db9dab0bcd4103f3653569f67b055dcb98e44
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-10-12 18:49:12 -0300

    add delete customer, api and front

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -1,8 +1,36 @@
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { authOptions } from "../../../lib/auth";
-import { error } from "console";
 import prismaClient from "../../../lib/prisma";
+
+export async function DELETE(request: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return NextResponse.json({ error: "Unauthorized " }, { status: 401 });
+  }
+
+  const { searchParams } = new URL(request.url);
+  const userId = searchParams.get("id");
+
+  if (!userId) {
+    return NextResponse.json({ error: "Client not found"}, { status: 404 });
+  }
+
+  try {
+    await prismaClient.customer.delete({ where: { id: userId as string } });
+
+    return NextResponse.json(
+      { message: "Cliente deletado com sucesso." },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Failed delete customer" },
+      { status: 401 }
+    );
+  }
+}
 
 export async function POST(request: Request) {
   const session = await getServerSession(authOptions);
--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,6 +1,19 @@
+"use client";
+
 import { CustomerProps } from "../../../../../utils/customer.type";
+import { api } from "../../../../../lib/api";
 
 export function CardCustomer({ customer }: { customer: CustomerProps }) {
+  async function handleDeleteCutomer() {
+    try {
+      const response = await api.delete("/api/customer", {
+        params: { id: customer.id },
+      });
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+  }
   return (
     <article className="flex flex-col bg-gray-100 border-2 p-2 rounded-lg gap-2 hover:scale-105 duration-200">
       <h2>
@@ -12,7 +25,10 @@ export function CardCustomer({ customer }: { customer: CustomerProps }) {
       <p>
         <b>Telefone:</b> {customer.phone}
       </p>
-      <button className="bg-red-500 px-4 rounded text-white mt-2 self-start">
+      <button
+        onClick={handleDeleteCutomer}
+        className="bg-red-500 px-4 rounded text-white mt-2 self-start"
+      >
         Deletar
       </button>
     </article>
