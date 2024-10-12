ad86da5a9c748d99bd5c95f0731e9781c97dec4e
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-10-12 16:55:52 -0300

    add post costumer route api

---
+++ src/app/api/customer/route.ts
@@ -0,0 +1,30 @@
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../../../lib/auth";
+import { error } from "console";
+import prismaClient from "../../../lib/prisma";
+
+export async function POST(request: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return NextResponse.json({ error: "Unauthorized " }, { status: 401 });
+  }
+
+  const { name, email, phone, address, userId } = await request.json();
+
+  try {
+    await prismaClient.customer.create({
+      data: { name, email, phone, address: address ? address : "", userId },
+    });
+
+    return NextResponse.json(
+      { message: "Cliente cadastro com sucesso!" },
+      { status: 400 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error while register new customer" },
+      { status: 501 }
+    );
+  }
+}
